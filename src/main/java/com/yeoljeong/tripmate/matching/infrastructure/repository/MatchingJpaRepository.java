30c2f74bd9cb3775229dee41f8a96316a6634511
sunsik17 <17sunsik@gmail.com>
2026-04-27 02:37:59 +0900

    chore: implement MatchingRepository interface in MatchingJpaRepository

--- src/main/java/com/yeoljeong/tripmate/matching/infrastructure/repository/MatchingJpaRepository.java
+++ src/main/java/com/yeoljeong/tripmate/matching/infrastructure/repository/MatchingJpaRepository.java
@@ -1,12 +1,13 @@
 package com.yeoljeong.tripmate.matching.infrastructure.repository;
 
+import com.yeoljeong.tripmate.matching.domain.repository.MatchingRepository;
 import com.yeoljeong.tripmate.matching.infrastructure.repository.jpa.SpringDataMatchingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class MatchingJpaRepository {
+public class MatchingJpaRepository implements MatchingRepository {
 
 	private final SpringDataMatchingRepository matchingRepository;
 
