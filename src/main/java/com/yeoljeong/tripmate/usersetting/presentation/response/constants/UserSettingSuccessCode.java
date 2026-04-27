fdd36596bb3f603f550a8bcb666205adc56a8e3d
sunsik17 <17sunsik@gmail.com>
2026-04-28 00:58:16 +0900

    chore: add error and success code enums for UserSetting module

---
+++ src/main/java/com/yeoljeong/tripmate/usersetting/presentation/response/constants/UserSettingSuccessCode.java
@@ -0,0 +1,21 @@
+package com.yeoljeong.tripmate.usersetting.presentation.response.constants;
+
+import com.yeoljeong.tripmate.response.constants.SuccessCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserSettingSuccessCode implements SuccessCode {
+	OK(HttpStatus.OK, "세팅이 수정되었습니다.")
+	;
+
+	private final HttpStatus status;
+	private final String message;
+
+	@Override
+	public int getCode() {
+		return this.status.value();
+	}
+}
