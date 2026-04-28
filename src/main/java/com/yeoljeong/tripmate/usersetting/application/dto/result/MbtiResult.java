551eaac5f4ab7ffbf8886696c661a42d9cc30f83
sunsik17 <17sunsik@gmail.com>
2026-04-28 14:04:56 +0900

    refactor: replace MBTI enums with Strings in UserSettingCommand and MbtiResult

--- src/main/java/com/yeoljeong/tripmate/usersetting/application/dto/result/MbtiResult.java
+++ src/main/java/com/yeoljeong/tripmate/usersetting/application/dto/result/MbtiResult.java
@@ -1,18 +1,19 @@
 package com.yeoljeong.tripmate.usersetting.application.dto.result;
 
 import com.yeoljeong.tripmate.usersetting.domain.entity.UserSetting;
-import com.yeoljeong.tripmate.usersetting.domain.entity.constants.MbtiIE;
-import com.yeoljeong.tripmate.usersetting.domain.entity.constants.MbtiPJ;
-import com.yeoljeong.tripmate.usersetting.domain.entity.constants.MbtiSN;
-import com.yeoljeong.tripmate.usersetting.domain.entity.constants.MbtiTF;
 
 public record MbtiResult(
-	MbtiIE ie,
-	MbtiSN sn,
-	MbtiTF tf,
-	MbtiPJ pj
+	String ie,
+	String sn,
+	String tf,
+	String pj
 ) {
 	public static MbtiResult from(UserSetting setting) {
-		return new MbtiResult(setting.getIe(), setting.getSn(), setting.getTf(), setting.getPj());
+		return new MbtiResult(
+			setting.getIe().name(),
+			setting.getSn().name(),
+			setting.getTf().name(),
+			setting.getPj().name()
+		);
 	}
 }
