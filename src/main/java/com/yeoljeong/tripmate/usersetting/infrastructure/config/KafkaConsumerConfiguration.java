e61b7bf4933a83e850b236f6f1c34fde13de9d0c
sunsik17 <17sunsik@gmail.com>
2026-04-30 00:06:19 +0900

    feat: configure Kafka consumer with manual ack, retry, and dead-letter handling

---
+++ src/main/java/com/yeoljeong/tripmate/usersetting/infrastructure/config/KafkaConsumerConfiguration.java
@@ -0,0 +1,47 @@
+package com.yeoljeong.tripmate.usersetting.infrastructure.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.common.TopicPartition;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.ContainerProperties.AckMode;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.ExponentialBackOff;
+import org.springframework.util.backoff.FixedBackOff;
+
+@Configuration
+@RequiredArgsConstructor
+public class KafkaConsumerConfiguration {
+
+	@Value("${spring.retry.interval}")
+	private long retryInterval;
+
+	@Value("${spring.retry.max-attempts}")
+	private int maxAttempts;
+
+	@Bean
+	public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
+		ConsumerFactory<String, Object> consumerFactory,
+		KafkaTemplate<String, Object> kafkaTemplate
+	) {
+		ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory);
+		factory.getContainerProperties().setAckMode(AckMode.MANUAL);
+		DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(
+			kafkaTemplate,
+			(record, ex) -> new TopicPartition(record.topic() + ".DLT", record.partition()));
+		ExponentialBackOff backOff = new ExponentialBackOff(retryInterval, 3.0);
+		backOff.setMaxAttempts(maxAttempts);
+
+		DefaultErrorHandler errorHandler = new DefaultErrorHandler(recoverer, backOff);
+		factory.setCommonErrorHandler(errorHandler);
+
+		return factory;
+	}
+
+}
