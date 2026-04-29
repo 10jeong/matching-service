ae003cf620dbc16416d02fe8d26e23f17f354ec4
sunsik17 <17sunsik@gmail.com>
2026-04-30 04:32:18 +0900

    chore: VO 클래스 접근 제어자 변경 및 생성 팩토리 메서드 정의

--- src/main/java/com/yeoljeong/tripmate/matching/domain/model/Location.java
+++ src/main/java/com/yeoljeong/tripmate/matching/domain/model/Location.java
@@ -4,15 +4,23 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import java.math.BigDecimal;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-class Location {
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Location {
 
 	@Column(nullable = false, precision = 10, scale = 7)
 	private BigDecimal lat;
 
 	@Column(nullable = false, precision = 10, scale = 7)
 	private BigDecimal lng;
+
+	public static Location of(BigDecimal lat, BigDecimal lng) {
+		return new Location(lat, lng);
+	}
 }
--- src/main/java/com/yeoljeong/tripmate/matching/domain/model/MatchingPeriod.java
+++ src/main/java/com/yeoljeong/tripmate/matching/domain/model/MatchingPeriod.java
@@ -1,14 +1,22 @@
 package com.yeoljeong.tripmate.matching.domain.model;
 
+import static com.yeoljeong.tripmate.matching.domain.exception.MatchingErrorCode.RECRUITMENT_DEADLINE_AFTER_ACTIVITY_DATE;
+
+import com.yeoljeong.tripmate.exception.BusinessException;
+import com.yeoljeong.tripmate.matching.domain.exception.MatchingErrorCode;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Embeddable
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-class MatchingPeriod {
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class MatchingPeriod {
 
 	@Column(nullable = false)
 	private LocalDateTime scheduledAt;
@@ -16,7 +24,14 @@ class MatchingPeriod {
 	@Column(nullable = false)
 	private LocalDateTime recruitDeadline;
 
-	public boolean isExpired() {
+	public static MatchingPeriod of(LocalDateTime scheduledAt, LocalDateTime recruitDeadline) {
+		if (recruitDeadline.isAfter(scheduledAt)) {
+			throw new BusinessException(RECRUITMENT_DEADLINE_AFTER_ACTIVITY_DATE);
+		}
+		return new MatchingPeriod(scheduledAt, recruitDeadline);
+	}
+
+	boolean isExpired() {
 		return LocalDateTime.now().isAfter(recruitDeadline);
 	}
 }
