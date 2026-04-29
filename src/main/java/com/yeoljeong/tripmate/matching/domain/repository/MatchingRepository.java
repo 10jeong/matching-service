10ec61134df355250823655dd055913c46097984
sunsik17 <17sunsik@gmail.com>
2026-04-30 04:26:01 +0900

    feat: 매칭 레포 save, exist 명세 정의 및 구현

--- src/main/java/com/yeoljeong/tripmate/matching/domain/repository/MatchingRepository.java
+++ src/main/java/com/yeoljeong/tripmate/matching/domain/repository/MatchingRepository.java
@@ -1,5 +1,9 @@
 package com.yeoljeong.tripmate.matching.domain.repository;
 
-public interface MatchingRepository {
+import com.yeoljeong.tripmate.matching.domain.model.Matching;
+import java.util.UUID;
 
+public interface MatchingRepository {
+	boolean existsByHostUserIdAndMatchingStatusOpen(UUID hostId);
+	Matching save(Matching matching);
 }
