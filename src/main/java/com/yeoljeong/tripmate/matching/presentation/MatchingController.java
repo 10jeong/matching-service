29b550ad6b2818f62b57cc2514ec21f6f85d24b0
sunsik17 <17sunsik@gmail.com>
2026-05-02 22:43:07 +0900

    feat: sse 구독 api 생성

--- src/main/java/com/yeoljeong/tripmate/matching/presentation/MatchingController.java
+++ src/main/java/com/yeoljeong/tripmate/matching/presentation/MatchingController.java
@@ -2,20 +2,24 @@ package com.yeoljeong.tripmate.matching.presentation;
 
 import static com.yeoljeong.tripmate.response.constants.CommonSuccessCode.CREATE;
 
-import com.yeoljeong.tripmate.auth.LoginUser;
-import com.yeoljeong.tripmate.auth.UserContext;
+import com.yeoljeong.tripmate.auth.annotation.LoginUser;
+import com.yeoljeong.tripmate.auth.context.UserContext;
 import com.yeoljeong.tripmate.matching.application.MatchingCommandService;
+import com.yeoljeong.tripmate.matching.application.external.MatchingSseManager;
 import com.yeoljeong.tripmate.matching.presentation.dto.request.CreateMatchingRequest;
 import com.yeoljeong.tripmate.matching.presentation.dto.response.MatchingDetailResponse;
 import com.yeoljeong.tripmate.response.ApiResponse;
 import jakarta.validation.Valid;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 @RequiredArgsConstructor
