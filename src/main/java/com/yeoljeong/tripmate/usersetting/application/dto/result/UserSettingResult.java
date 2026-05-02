068f1b1f5d351a089d93f98b02bbabd66e40a6de
sunsik17 <17sunsik@gmail.com>
2026-05-02 22:57:53 +0900

    chore: 사용하지 않는 dto 모두 삭제 후 domain layer enum반환으로 변경

--- src/main/java/com/yeoljeong/tripmate/usersetting/application/dto/result/UserSettingResult.java
+++ src/main/java/com/yeoljeong/tripmate/usersetting/application/dto/result/UserSettingResult.java
@@ -2,20 +2,30 @@ package com.yeoljeong.tripmate.usersetting.application.dto.result;
 
 import com.yeoljeong.tripmate.usersetting.domain.model.UserSetting;
 import com.yeoljeong.tripmate.usersetting.domain.model.constants.Gender;
+import com.yeoljeong.tripmate.usersetting.domain.model.constants.MbtiIE;
+import com.yeoljeong.tripmate.usersetting.domain.model.constants.MbtiPJ;
+import com.yeoljeong.tripmate.usersetting.domain.model.constants.MbtiSN;
+import com.yeoljeong.tripmate.usersetting.domain.model.constants.MbtiTF;
 import java.util.UUID;
 
 public record UserSettingResult (
 	UUID userId,
 	Gender gender,
 	boolean isSmoking,
-	MbtiResult mbti
+	MbtiIE mbtiIE,
+	MbtiSN mbtiSN,
+	MbtiTF mbtiTF,
+	MbtiPJ mbtiPJ
 ){
 	public static UserSettingResult from(UserSetting setting) {
 		return new UserSettingResult(
 			setting.getUserId(),
 			setting.getGender(),
 			setting.isSmoking(),
-			MbtiResult.from(setting)
+			setting.getIe(),
+			setting.getSn(),
+			setting.getTf(),
+			setting.getPj()
 		);
 	}
 }
--- src/main/java/com/yeoljeong/tripmate/usersetting/presentation/dto/response/UserSettingResponse.java
+++ src/main/java/com/yeoljeong/tripmate/usersetting/presentation/dto/response/UserSettingResponse.java
@@ -1,20 +1,30 @@
 package com.yeoljeong.tripmate.usersetting.presentation.dto.response;
 
 import com.yeoljeong.tripmate.usersetting.application.dto.result.UserSettingResult;
+import com.yeoljeong.tripmate.usersetting.domain.model.constants.MbtiIE;
+import com.yeoljeong.tripmate.usersetting.domain.model.constants.MbtiPJ;
+import com.yeoljeong.tripmate.usersetting.domain.model.constants.MbtiSN;
+import com.yeoljeong.tripmate.usersetting.domain.model.constants.MbtiTF;
 import java.util.UUID;
 
 public record UserSettingResponse(
 	UUID userId,
 	String gender,
 	boolean isSmoking,
-	MbtiResponse mbti
+	MbtiIE mbtiIE,
+	MbtiSN mbtiSN,
+	MbtiTF mbtiTF,
+	MbtiPJ mbtiPJ
 ) {
 	public static UserSettingResponse from(UserSettingResult result) {
 		return new UserSettingResponse(
 			result.userId(),
 			result.gender().name(),
 			result.isSmoking(),
-			MbtiResponse.from(result.mbti())
+			result.mbtiIE(),
+			result.mbtiSN(),
+			result.mbtiTF(),
+			result.mbtiPJ()
 		);
 	}
 }
