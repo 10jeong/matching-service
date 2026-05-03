7afb1b67e3c97d86472b05d8ccf84df7ec8d6cf6
sunsik17 <17sunsik@gmail.com>
2026-05-03 17:18:53 +0900

    chore: audit 기능 사용 설정 추가

--- src/main/java/com/yeoljeong/tripmate/MatchingServiceApplication.java
+++ src/main/java/com/yeoljeong/tripmate/MatchingServiceApplication.java
@@ -2,8 +2,10 @@ package com.yeoljeong.tripmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "userAuditorAware")
 public class MatchingServiceApplication {
 
 	public static void main(String[] args) {
