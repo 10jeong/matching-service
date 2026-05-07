14d52317521ec67f91a7e114f51c57ff59fc548d
sunsik17 <17sunsik@gmail.com>
2026-05-07 15:57:53 +0900

    feat: 사용하지 않는 import 삭제

--- src/main/java/com/yeoljeong/tripmate/usersetting/application/usecase/FindEnableMatchingUserUsecase.java
+++ src/main/java/com/yeoljeong/tripmate/usersetting/application/usecase/FindEnableMatchingUserUsecase.java
@@ -1,7 +1,6 @@
 package com.yeoljeong.tripmate.usersetting.application.usecase;
 
 import com.yeoljeong.tripmate.usersetting.application.dto.command.MatchingCandidateCriteria;
-import java.security.NoSuchAlgorithmException;
 
 public interface FindEnableMatchingUserUsecase {
 	void findAllEnableMatchingUser(MatchingCandidateCriteria criteria);
