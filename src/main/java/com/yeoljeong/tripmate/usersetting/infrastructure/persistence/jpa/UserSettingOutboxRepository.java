8346a58c168218195b31a94e1e624a0ca0fa3c7f
sunsik17 <17sunsik@gmail.com>
2026-05-07 15:54:23 +0900

    feat: 아웃박스 jpa 저장소 추가

---
+++ src/main/java/com/yeoljeong/tripmate/usersetting/infrastructure/persistence/jpa/UserSettingOutboxRepository.java
@@ -0,0 +1,11 @@
+package com.yeoljeong.tripmate.usersetting.infrastructure.persistence.jpa;
+
+import com.yeoljeong.tripmate.domain.constants.OutboxStatus;
+import com.yeoljeong.tripmate.usersetting.infrastructure.persistence.UserSettingOutbox;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserSettingOutboxRepository extends JpaRepository<UserSettingOutbox, UUID> {
+	List<UserSettingOutbox> findAllByStatus(OutboxStatus status);
+}
