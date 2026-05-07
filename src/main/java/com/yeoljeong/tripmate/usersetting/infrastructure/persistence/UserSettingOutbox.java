238efeb0a499283f510f4ee00a210c71fbec2c7a
sunsik17 <17sunsik@gmail.com>
2026-05-07 15:45:36 +0900

    feat: @Getter 추가

--- src/main/java/com/yeoljeong/tripmate/usersetting/infrastructure/persistence/UserSettingOutbox.java
+++ src/main/java/com/yeoljeong/tripmate/usersetting/infrastructure/persistence/UserSettingOutbox.java
@@ -1,4 +1,4 @@
-package com.yeoljeong.tripmate.usersetting.domain.model;
+package com.yeoljeong.tripmate.usersetting.infrastructure.persistence;
 
 import com.yeoljeong.tripmate.domain.Outbox;
 import jakarta.persistence.Entity;
@@ -7,9 +7,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @Table(name = "user_setting_outbox")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserSettingOutbox extends Outbox {
 
 	@Id
