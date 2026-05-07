0762e1ecc0624b4994fb34c964ceb0100bdc4506
sunsik17 <17sunsik@gmail.com>
2026-05-07 15:43:12 +0900

    chore: 사용하지 않는 eventPublisher 삭제

--- src/main/java/com/yeoljeong/tripmate/matching/application/external/MatchingEventPublisher.java
+++
@@ -1,9 +0,0 @@
-package com.yeoljeong.tripmate.matching.application.external;
-
-import com.yeoljeong.tripmate.matching.domain.model.Matching;
-import java.security.NoSuchAlgorithmException;
-
-public interface MatchingEventPublisher {
-	void publishMatchingCreated(Matching matching) throws NoSuchAlgorithmException;
-	void publishMatchingAccomplished(Matching matching);
-}
