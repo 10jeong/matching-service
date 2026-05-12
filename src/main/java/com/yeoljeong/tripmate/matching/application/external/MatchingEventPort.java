68b7591ea61a548ad6999583cc8a8cd7e019273d
sunsik17 <17sunsik@gmail.com>
2026-05-13 02:14:16 +0900

    feat: MatchingEventPort에 appendMateSubscribed, appendMateUnsubscribed 추가

--- src/main/java/com/yeoljeong/tripmate/matching/application/external/MatchingEventPort.java
+++ src/main/java/com/yeoljeong/tripmate/matching/application/external/MatchingEventPort.java
@@ -1,8 +1,11 @@
 package com.yeoljeong.tripmate.matching.application.external;
 
 import com.yeoljeong.tripmate.matching.domain.model.Matching;
+import java.util.UUID;
 
 public interface MatchingEventPort {
 	void appendMatchingCreated(Matching matching);
 	void appendMatchingAccomplished(Matching matching);
+	void appendMateSubscribed(UUID userId);
+	void appendMateUnsubscribed(UUID userId);
 }
