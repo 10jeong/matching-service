52257f2260cb173c36329c4b200e28119c71003a
sunsik17 <17sunsik@gmail.com>
2026-05-14 01:29:56 +0900

    feat: add internal API for withdrawal check in MatchingInternalController

---
+++ src/main/java/com/yeoljeong/tripmate/matching/presentation/ineternal/MatchingInternalController.java
@@ -0,0 +1,23 @@
+package com.yeoljeong.tripmate.matching.presentation.ineternal;
+
+import com.yeoljeong.tripmate.matching.application.usecase.MatchingQueryService;
+import jakarta.ws.rs.PathParam;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/internal")
+@RequiredArgsConstructor
+public class MatchingInternalController {
+
+	private final MatchingQueryService queryService;
+
+	@GetMapping("/matching/withdawal-check")
+	public boolean withdrawCheck(
+		@PathParam("userId") UUID userId
+	) {
+		return queryService.withdrawCheck(userId);
+	}
+
+}
