d24f5ae05199f54ae92c4ed32644cbc5b16f7e80
sunsik17 <17sunsik@gmail.com>
2026-05-14 12:25:26 +0900

    feat: add latitude and longitude fields to matching criteria for geolocation support

--- src/main/java/com/yeoljeong/tripmate/matching/presentation/dto/request/UserMatchingCriteriaRequest.java
+++ src/main/java/com/yeoljeong/tripmate/matching/presentation/dto/request/UserMatchingCriteriaRequest.java
@@ -1,11 +1,16 @@
 package com.yeoljeong.tripmate.matching.presentation.dto.request;
 
 import com.yeoljeong.tripmate.matching.application.dto.command.UserMatchingCriteriaCommand;
+import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 
 public record UserMatchingCriteriaRequest (
 	String gender,
 	boolean isSmoking,
+	@NotNull
+	Double latitude,
+	@NotNull
+	Double longitude,
 	String mbtiIE,
 	String mbtiSN,
 	String mbtiTF,
@@ -23,6 +28,8 @@ public record UserMatchingCriteriaRequest (
 			userId,
 			gender,
 			isSmoking,
+			latitude,
+			longitude,
 			mbtiIE,
 			mbtiSN,
 			mbtiTF,
